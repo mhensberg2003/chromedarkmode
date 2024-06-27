2d2a7dc68ab7c043355ee52272fb6f6d1bc2bb86
mhensberg2003 <mhensberg2003@gmail.com>
2024-06-27 14:11:46 +0200

    Added a few QOL changes

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ const darkMode = `
     hmtl, body, div, span, applet, object, iframe,
     h1, h2, h3, h4, h5, h6, p, blockquote, pre,
     a, abbr, acronym, address, big, cite, code,
-    del, dfn, em, img, ins, kbd, q, s, samp,
+    del, dfn, em, ins, kbd, q, s, samp,
     small, strike, strong, sub, sup, tt, var,
     b, u, i, center,
     dl, dt, dd, ol, ul, li,
@@ -11,7 +11,7 @@ const darkMode = `
     article, aside, canvas, details, embed,
     figure, figcaption, footer, header, hgroup,
     menu, nav, output, ruby, section, summary,
-    time, mark, audio, video {
+    time, mark, audio {
         background-color: #121212 !important;
         color: #e0e0e0 !important;
         border-color: #333 !important;
